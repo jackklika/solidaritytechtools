393897d1f6575266b1a74f5e14d7aefe49c3d075
J <7218605+jackklika@users.noreply.github.com>
2026-08-21 16:20:18 -0500

    export matching, membership and normalize helpers from the package root

--- solidaritytechtools/__init__.py
+++ solidaritytechtools/__init__.py
@@ -12,8 +12,27 @@ from solidaritytechtools.match_persons.match_persons import (
     find_matches_emails,
     match_persons,
 )
+from solidaritytechtools.matching import (
+    ContactIndex,
+    ContactKeys,
+    ContactMatch,
+    MatchStrategy,
+    contact_keys,
+    keys_from_mapping,
+    keys_from_person,
+    keys_from_user,
+    match_contacts,
+    prefer_member_then_newest,
+)
 from solidaritytechtools.services.users import UserStore, get_all_users, set_email_permission
 from solidaritytechtools.tools.add_traffic_data import add_traffic_data, build_traffic_scorer
+from solidaritytechtools.utils.membership import (
+    get_join_date,
+    get_membership_status,
+    get_membership_type,
+    is_member_in_good_standing,
+)
+from solidaritytechtools.utils.normalize import normalize_name, normalize_phone, normalize_zip
 
 __all__ = [
     "STClient",
@@ -24,9 +43,26 @@ __all__ = [
     "find_matches",
     "find_best_match",
     "find_matches_emails",
+    "ContactIndex",
+    "ContactKeys",
+    "ContactMatch",
+    "MatchStrategy",
+    "contact_keys",
+    "match_contacts",
+    "keys_from_user",
+    "keys_from_person",
+    "keys_from_mapping",
+    "prefer_member_then_newest",
     "get_all_users",
     "UserStore",
     "set_email_permission",
     "add_traffic_data",
     "build_traffic_scorer",
+    "get_membership_status",
+    "get_membership_type",
+    "get_join_date",
+    "is_member_in_good_standing",
+    "normalize_phone",
+    "normalize_name",
+    "normalize_zip",
 ]
